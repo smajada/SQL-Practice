1716690d579abad2ce0504de7ffc084a08a82f6f
Sergio Majada <smajadamanresa@cifpfbmoll.eu>
2023-03-02 21:35:59 +0100

    Updated 05_07

--- Material/Unit 05/05_07_MATE_Northwind.sql
+++ Material/Unit 05/05_07_MATE_Northwind.sql
@@ -1,4 +1,5 @@
---1.- Show total cost of the four most expensive orders in May 1998. Clue: price*quantity*(1-discount). 
+--1.- Show total cost of the four most expensive orders in May 1998. 
+-- Clue: price*quantity*(1-discount). 
 -- +---------+------------+
 -- | OrderID | total_cost |
 -- +---------+------------+
@@ -7,10 +8,24 @@
 -- |   11068 |  4650.3600 |
 -- |   11070 |  3653.3250 |
 -- +---------+------------+
-select orderID, sum()
+SELECT
+    orders.orderID,
+    sum(UnitPrice * quantity *(1 - discount)) AS total_cost
+FROM
+    `order details`,
+    orders
+WHERE
+    orders.orderid = `order details`.orderid
+    AND MONTH(orderdate) = 05
+    AND year(orderdate) = 1998
+GROUP BY
+    orders.orderid
+ORDER BY
+    total_cost DESC
+LIMIT
+    4;
 
 --2.- Show the total sales by year. 
-
 -- +------+----------------+
 -- | year | sales_per_year |
 -- +------+----------------+
@@ -18,7 +33,16 @@ select orderID, sum()
 -- | 1997 |   1283858.0625 |
 -- | 1998 |    915791.1360 |
 -- +------+----------------+
-
+SELECT
+    year(orderdate) year,
+    sum(UnitPrice * quantity *(1 - discount)) sales_per_year
+FROM
+    `order details`,
+    orders
+WHERE
+    orders.orderid = `order details`.orderid
+GROUP BY
+    year(orderdate);
 
 -- 3.- Total sales by country. Order by country and show the first 4 results.
 -- +-------------+----------------+
@@ -29,7 +53,20 @@ select orderID, sum()
 -- | Belgium     |     68513.2110 |
 -- | Brazil      |    223697.4870 |
 -- +-------------+----------------+
-
+SELECT
+    ShipCountry,
+    sum(UnitPrice * quantity *(1 - discount)) sales_per_year
+FROM
+    `order details`,
+    orders
+WHERE
+    orders.orderid = `order details`.orderid
+GROUP BY
+    ShipCountry
+ORDER BY
+    ShipCountry
+LIMIT
+    4;
 
 -- 4.- Show employee sales order by sales amount descendent. Show the first four results.
 -- +------------+--------------------+
@@ -40,7 +77,20 @@ select orderID, sum()
 -- |          1 |        393351.2700 |
 -- |          2 |        346611.0570 |
 -- +------------+--------------------+
-
+SELECT
+    employeeid,
+    sum(UnitPrice * quantity *(1 - discount)) sales_per_employee
+FROM
+    `order details`,
+    orders
+WHERE
+    orders.orderid = `order details`.orderid
+GROUP BY
+    employeeid
+ORDER BY
+    sales_per_employee DESC
+LIMIT
+    4;
 
 -- 5.- Show employee sales by country. Order by country ascendent and sales amount descendent. Show the first 20 rows.
 -- +-------------+------------+-------------+
@@ -67,9 +117,24 @@ select orderID, sum()
 -- | Belgium     |          7 |   9050.9250 |
 -- | Belgium     |          9 |   6379.4250 |
 -- +-------------+------------+-------------+
-
-
-
+SELECT
+    Orders.ShipCountry, Employees.EmployeeID,
+    sum(UnitPrice * quantity *(1 - discount)) salesAmount
+FROM
+    `order details`,
+    orders,
+    employees
+WHERE
+    orders.orderid = `order details`.orderid
+    AND Employees.EmployeeID = Orders.EmployeeID
+GROUP BY
+    Orders.ShipCountry,
+    Employees.EmployeeID
+ORDER BY
+    ShipCountry ASC,
+    salesAmount DESC
+LIMIT
+    20;
 
 -- 6.- Sales per category. Order by sales amount, descending. Show the first four results.
 -- +------------+----------------+-------------+
@@ -80,6 +145,22 @@ select orderID, sum()
 -- |          6 | Meat/Poultry   | 346658.2275 |
 -- |          3 | Confections    | 344943.0855 |
 -- +------------+----------------+-------------+
+
+select
+categories.CategoryID,
+categories.CategoryName,
+sum(`order details`.UnitPrice * quantity *(1 - discount)) salesAmount
+from
+categories,
+products,
+`order details`
+where
+    `order details`.ProductID = products.ProductID
+    and products.CategoryID = categories.CategoryID
+
+group by CategoryID
+order by salesAmount desc
+limit 4;
 
 
 -- 7.- Number of products by category. Order by number of products, descending. Show the first four lines.
@@ -92,6 +173,21 @@ select orderID, sum()
 -- |          2 | Condiments   |           12 |
 -- +------------+--------------+--------------+
 
+select
+categories.CategoryID,
+categories.CategoryName,
+count(*) num_products
+from
+categories,
+products
+where
+    products.CategoryID = categories.CategoryID
+
+group by CategoryID
+order by num_products desc
+limit 4;
+
+
 -- 8.- Order details of the order number 10248.
 -- +-----------+-----------+----------+----------+
 -- | ProductID | UnitPrice | Quantity | Discount |
@@ -101,12 +197,21 @@ select orderID, sum()
 -- |        72 |   67.8600 |        5 |     0.10 |
 -- +-----------+-----------+----------+----------+
 
+select ProductID, UnitPrice, Quantity, discount
+from `order details`
+where orderID = 10248;
+
 -- 9.- Total amount for order 10248. 
 -- +--------------+
 -- | total_amount |
 -- +--------------+
 -- |     772.2000 |
 -- +--------------+
+
+select
+sum(UnitPrice * quantity *(1 - discount)) total_amount
+from `order details`
+where orderID = 10248;
 
 
 -- 10.- List the employee last name and the customer company name for order 10248. 
@@ -115,6 +220,18 @@ select orderID, sum()
 -- +----------+---------------------------+
 -- | Buchanan | Vins et alcools Chevalier |
 -- +----------+---------------------------+
+
+SELECT
+    LastName,
+    CompanyName
+FROM
+    employees,
+    orders,
+    customers
+WHERE
+    orders.orderID = 10248
+    AND orders.customerID = customers.customerID
+    AND orders.employeeid = employees.employeeid;
 
 -- 11.- Top 10 products with more units sold. 
 -- +------------------------+------------+
@@ -132,6 +249,15 @@ select orderID, sum()
 -- | Flotemysost            |       1057 |
 -- +------------------------+------------+
 
+SELECT productName, SUM(`order details`.Quantity) units_sold
+from 
+`order details`,
+products
+where `order details`.ProductID = products.ProductID
+group by productName
+order by units_sold desc
+limit 10;
+
 -- 12.- Show the distinct buy-sell prices for the products. That is, the product id, the price at which it was bought and the price at which it was sold. Show only distinct values. Order by product id. List the first four lines. The buy price can be found in the Products table and the sell price can be found in the Order Details tables.
 -- +-----------+-----------+------------+
 -- | ProductID | buy_price | sell_price |
@@ -142,6 +268,8 @@ select orderID, sum()
 -- |         2 |   19.0000 |    37.0500 |
 -- +-----------+-----------+------------+
 
+
+
 -- 13.- Four most expensive products. 
 -- +------------------------+-----------+
 -- | ProductName            | UnitPrice |
@@ -151,9 +279,11 @@ select orderID, sum()
 -- | Mishi Kobe Niku        |   97.0000 |
 -- | Sir Rodney's Marmalade |   81.0000 |
 -- +------------------------+-----------+
+
+
+
 -- 14.- Show the distinct tuples of (ProductName, year, profit_per unit) for the products that gave more profit per unit. Year is the year the product was sold. Profit_per_unit is the difference between the price at which the product was sold and the price at which the product was bought.
 -- Show the first 6 lines.
-
 -- +------------------------+------+-----------------+
 -- | ProductName            | year | profit_per_unit |
 -- +------------------------+------+-----------------+
@@ -164,6 +294,8 @@ select orderID, sum()
 -- | Thringer Rostbratwurst | 1997 |        117.6005 |
 -- | Thringer Rostbratwurst | 1998 |        117.6005 |
 -- +------------------------+------+-----------------+
+
+
 
 -- 15. Top ten products in profit in sales. The profit in sales for a product is the sum of all the differences of the price paid for the product and the price charged for a product times the quantity of products sold.
 -- +------------------------+----------------+
@@ -180,6 +312,8 @@ select orderID, sum()
 -- | Carnarvon Tigers       |     28322.5000 |
 -- | Rssle Sauerkraut       |     23203.9200 |
 -- +------------------------+----------------+
+
+
 
 -- 16.- Products above average price (ordered by price descendent). 
 -- +---------------------------------+
